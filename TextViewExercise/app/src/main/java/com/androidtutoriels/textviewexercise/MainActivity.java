025312c891d208f93c781bc3feceaf0cc4e2fd25
benjamin94 <benjamin.lize94@gmail.com>
2017-04-07 16:00:35 +0100

    TextView Exercise

---
+++ TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
@@ -0,0 +1,17 @@
+package com.androidtutoriels.textviewexercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView reussi = (TextView)findViewById(R.id.le_texte);
+        reussi.setText("J'ai réussi! ouais!");
+    }
+}
