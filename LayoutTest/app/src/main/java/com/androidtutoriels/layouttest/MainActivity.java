5366243b1cc2b73b0d3010578d1a1c47fc813ee1
benjamin94 <benjamin.lize94@gmail.com>
2017-04-07 15:49:43 +0100

    TextView Java

--- LayoutTest/app/src/main/java/com/androidtutoriels/layouttest/MainActivity.java
+++ LayoutTest/app/src/main/java/com/androidtutoriels/layouttest/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidtutoriels.layouttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView texte = (TextView)findViewById(R.id.textView);
+        texte.setText("Voici le texte depuis activité Java");
+
     }
 }
