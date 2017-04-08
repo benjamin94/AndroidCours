0d4a182caf95caa74c7548bd562b3285202369fe
benjamin94 <benjamin.lize94@gmail.com>
2017-04-08 15:05:27 +0100

    ImageView Java Ex

--- TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
+++ TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidtutoriels.textviewexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
         TextView reussi = (TextView)findViewById(R.id.le_texte);
         reussi.setText("J'ai réussi! ouais!");
+
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+        image.setImageResource(R.drawable.android1);
+
     }
 }
