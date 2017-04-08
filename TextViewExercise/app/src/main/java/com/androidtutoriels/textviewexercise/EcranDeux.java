4cec836ce5f23f6e0553bb21129e3aa084ff887f
benjamin94 <benjamin.lize94@gmail.com>
2017-04-08 22:11:37 +0100

    Aller au deuxième écran Ex

--- TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/EcranDeux.java
+++ TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/EcranDeux.java
@@ -1,7 +1,10 @@
 package com.androidtutoriels.textviewexercise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EcranDeux extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class EcranDeux extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecran_deux);
+
+        final Intent intent = new Intent(this,MainActivity.class);
+
+        Button button = (Button)findViewById(R.id.nouvel_ecran_b);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
