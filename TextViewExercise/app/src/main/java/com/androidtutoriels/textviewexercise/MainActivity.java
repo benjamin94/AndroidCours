bfb5d11a2a76c551468d3bb1c939e241aba76610
benjamin94 <benjamin.lize94@gmail.com>
2017-04-08 21:05:55 +0100

    Log

--- TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
+++ TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidtutoriels.textviewexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String text = String.valueOf(editText.getText());
                 reussi.setText(text);
+                Log.i("MainActivity","bouton cliqué");
             }
         });
 
