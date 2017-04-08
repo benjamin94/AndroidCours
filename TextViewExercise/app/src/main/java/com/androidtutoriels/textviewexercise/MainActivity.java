3e23153e7cde8ab030cfff2418ebc5f78163999c
benjamin94 <benjamin.lize94@gmail.com>
2017-04-08 15:32:28 +0100

    Bouton Layout

--- TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
+++ TextViewExercise/app/src/main/java/com/androidtutoriels/textviewexercise/MainActivity.java
@@ -2,21 +2,37 @@ package com.androidtutoriels.textviewexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView reussi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView reussi = (TextView)findViewById(R.id.le_texte);
+        reussi = (TextView)findViewById(R.id.le_texte);
         reussi.setText("J'ai réussi! ouais!");
 
         ImageView image = (ImageView)findViewById(R.id.imageView);
         image.setImageResource(R.drawable.android1);
 
+        Button button = (Button)findViewById(R.id.button);
+
     }
+
+    public void onClic(View view){
+        reussi.setText("J'ai cliqué sur le bouton");
+    }
+
+
+
+
+
+
 }
