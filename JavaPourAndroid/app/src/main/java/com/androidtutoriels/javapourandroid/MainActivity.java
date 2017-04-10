77159d66a17396cbe8c8472bd859e033e5e3c271
benjamin94 <benjamin.lize94@gmail.com>
2017-04-10 18:48:39 +0100

    Methodes Ex

--- JavaPourAndroid/app/src/main/java/com/androidtutoriels/javapourandroid/MainActivity.java
+++ JavaPourAndroid/app/src/main/java/com/androidtutoriels/javapourandroid/MainActivity.java
@@ -15,15 +15,23 @@ public class MainActivity extends AppCompatActivity {
         int a = 5;
         int b = 7;
 
-        int c = sommeDeAEtB(a,b);
+        int c = sommeEtLog(a,b);
 
         Log.i("Resultat", "c = " + c);
-
     }
 
     public int sommeDeAEtB(int a, int b){
         int c = a + b;
         return c;
+    }
+    public void logJeSuisContent(){
+        Log.i("Resultat", "je suis content");
+
+    }
+    public int sommeEtLog(int a, int b){
+        int d = sommeDeAEtB(a,b);
+        logJeSuisContent();
+        return d;
     }
 
 }
