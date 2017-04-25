895641266cdee822f2a0911d2163cfa3747c4df8
benjamin94 <benjamin.lize94@gmail.com>
2017-04-25 22:18:23 +0100

    RecyclerView

--- Meteos/app/src/main/java/com/androidtutoriels/meteos/MainActivity.java
+++ Meteos/app/src/main/java/com/androidtutoriels/meteos/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -40,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     TextView  temperature_item;
     ImageView image_item;
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         jour_item    = (TextView)findViewById(R.id.jour_tv);
         temperature_item = (TextView)findViewById(R.id.temperature_tv);
         image_item    = (ImageView) findViewById(R.id.icon_iv);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
 
         new Request5Jour().execute();
         //new TestRequest().execute();
